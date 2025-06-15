cd4066f7011f49cc6f4d99463990491a0ad81486
rxManuel <50890620+rxManuel@users.noreply.github.com>
2025-06-15 13:56:37 -0600

    Add Parallel version for prime number detector

---
+++ paradigms-for-interview-problems/src/main/java/primenumber/declarative/Parallel.java
@@ -0,0 +1,20 @@
+package primenumber.declarative;
+
+import primenumber.PrimeNumberDetector;
+
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class Parallel implements PrimeNumberDetector {
+    @Override
+    public boolean isPrime(Integer n) {
+        /*
+        IntPredicate tryMeLambda = m -> {
+            System.out.println( m + " " + Thread.currentThread().getName());
+            return n % m == 0;
+        };*/
+        return (n > 1) &&
+                // no "+ 1" because rangeClosed                                // lambda on fly
+                IntStream.rangeClosed(2, (int) Math.sqrt(n)).parallel().filter(m -> n%m == 0).count() == 0;
+    }
+}
--- paradigms-for-interview-problems/src/test/java/primenumber/DeclarativePrimeNumberDetectorTest.java
+++ paradigms-for-interview-problems/src/test/java/primenumber/DeclarativePrimeNumberDetectorTest.java
@@ -3,6 +3,7 @@ package primenumber;
 import org.junit.Test;
 import primenumber.declarative.Classical;
 import primenumber.declarative.Dynamic;
+import primenumber.declarative.Parallel;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -29,5 +30,16 @@ public class DeclarativePrimeNumberDetectorTest {
         assertTrue( dynamic.isPrime(29) );
         assertTrue( dynamic.isPrime(307) );
         assertTrue( dynamic.isPrime(701) );
+    }
+
+    @Test
+    public void parallelIsPrime() {
+        PrimeNumberDetector parallel = new Parallel();
+        assertTrue( parallel.isPrime(7));
+        assertFalse( parallel.isPrime(1) );
+        assertFalse( parallel.isPrime(-1) );
+        assertTrue( parallel.isPrime(29) );
+        assertTrue( parallel.isPrime(307) );
+        assertTrue( parallel.isPrime(701) );
     }
 }
